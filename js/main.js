76faba8e8ade7d57e0ddd31702d28a09a9eb074e
Alo Ndubuisi <93560628+AloNdubuisi@users.noreply.github.com>
2023-04-09 23:55:44 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// var insertUser = document.getElementById('insert');
+//       var updateUser = document.getElementById('update');
+//       var deleteUser = document.getElementById('delete');
+//       var insertUser = document.getElementById('insert');
+//       var emailId = document.getElementById('emailid');
+//       var userName = document.getElementById('username');
+//       var message = document.getElementById('message'); 
+//       var searchUser = document.getElementById('search-btn');
+//       var findUserName = document.getElementById('findusername');
+//       var findUserEmail = document.getElementById('finduseremail');
+//       var findUserMessage = document.getElementById('findusermessage');
+      
+
+
+//       function InsertUser(){
+//         set(ref(db, "user/"+ userName.value),{
+//             UserName: userName.value,
+//             EmailId: emailId.value,
+//             Message: message.value
+//         }).then(()=>{
+//             alert("Details submitted successfully !");
+//         }).then((error)=>{
+//             alert(error);
+//         })
+        
+//       }
+//       function FindData(){
+        
+//       }
+//       function UpdateUser(){
+        
+//       }
+//       function DeleteUser(){
+        
+//       }
+
+//       insertUser.addEventListener('click', InsertUser);
+//       findUserName.addEventListener('click', FindData);
+//       updateUser.addEventListener('click', UpdateUser);
+//       deleteUser.addEventListener('click', DeleteUser);
